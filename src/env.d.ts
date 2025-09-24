d2bc6b2b82f090d2d56d604442669d36ac616acf
El Houcine Qara <elhoucine.qara@harx.ai>
2025-09-24 06:34:10 +0100

    feat: refactor RepMatchingPanel with new layout and harx theme

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
     readonly VITE_QIANKUN: string;
     readonly VITE_API_URL_CALL: string;
     readonly VITE_HIDE_SECTIONS: string;
+    readonly VITE_API_URL_GIGS: string;
   }
   
   interface ImportMeta {
